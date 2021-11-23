56cad6d5480701b8fa03af921c778617c5679209
TrungN168 <84520413+TrungN168@users.noreply.github.com>
2021-11-23 10:04:47 -0800

    change_folder

---
+++ Shell_Basic/myscript.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+# ECHO COMMAND
+echo Hello World!
+
+#VARIABLES
+#Uppercase by convention
+#Letters, numbers, underscores
+NAME="TrungV"
+#echo "My name is ${NAME}"
+
+#USER INPUT
+read -p "Enter your name: " NAME
+echo "Hello ${NAME}, nice to meet you!"
+
+# SIMPLE IF STATEMENT
+if [ "$NAME" == "Trung_V" ]
+then
+    echo "Your name is Trung"
+else 
+    echo "Your name is not Trung"
+fi
+
+# ELSE-IF (elif)
+if [ "$NAME" == "Trung_V" ]
+then
+    echo "Your name is Trung"
+elif [ "$NAME" == "Luis" ]
+then  
+    echo "Your name is Luis"
+else 
+    echo "Your name is NOT TRUNG or LUIS"
+fi
+
+# FOR LOP 
+for i in 1 2 3 4 5
+do
+echo "Hello ${NAME} $i"
+done
+
+# While Loop
+
+i=1
+while [ $i -le 5 ]
+do
+    echo "Hello World $i"
+    i=`expr $i + 1`
+done
+
+a=1
+if [ $a -le 5 ]
+then
+    echo "$a < 5"
+    a=`expr $a + 1`
+else
+    echo "$a is not less than 5"
+fi
